9df949cf771bc0363278de368a8b26fceaa481ff
Khatthaphone <khatthaphonez@gmail.com>
2016-03-15 22:43:55 +0700

    created arrays xml to store text for listview

--- app/src/main/java/com/khatthaphone/code/physix/MainActivity.java
+++ app/src/main/java/com/khatthaphone/code/physix/MainActivity.java
@@ -2,6 +2,8 @@ package com.khatthaphone.code.physix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String pageName[] = getResources().getStringArray(R.array.pageName);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,pageName );
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
     }
 }
