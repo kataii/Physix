4aed810f6801a1439149b2ea09db03cd48ef751a
Khatthaphone <khatthaphonez@gmail.com>
2016-03-16 19:59:22 +0700

    Update on Adapter, fixed image inflating

--- app/src/main/java/com/khatthaphone/code/physix/MainActivity.java
+++ app/src/main/java/com/khatthaphone/code/physix/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String pageName[] = getResources().getStringArray(R.array.pageName);
+        final String pageName[] = getResources().getStringArray(R.array.pageName);
 
-        int[] imageName = getResources().getIntArray(R.array.imageName);
+        int[] imageName = {
+                R.drawable.arrows,
+                R.drawable.arrows,
+                R.drawable.arrows,
+                R.drawable.arrows,
+                R.drawable.arrows,
+                R.drawable.arrows
+        };
 
         CustomAdapter adapter = new CustomAdapter(getApplicationContext(), pageName, imageName);
 
@@ -27,7 +35,31 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+                Toast.makeText(getBaseContext(), pageName[position], Toast.LENGTH_SHORT).show();
+
+                switch (position) {
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    default:
+
+                        break;
+                }
             }
         });
     }
