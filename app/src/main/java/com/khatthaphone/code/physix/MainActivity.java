32d3ee494f46589e059e311d7f44214b4594c638
Khatthaphone <khatthaphonez@gmail.com>
2016-03-16 13:41:51 +0700

    Add Custom Adapter

--- app/src/main/java/com/khatthaphone/code/physix/MainActivity.java
+++ app/src/main/java/com/khatthaphone/code/physix/MainActivity.java
@@ -2,6 +2,8 @@ package com.khatthaphone.code.physix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,8 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String pageName[] = getResources().getStringArray(R.array.pageName);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,pageName );
+
+        int[] imageName = getResources().getIntArray(R.array.imageName);
+
+        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), pageName, imageName);
+
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,pageName );*/
+
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
     }
 }
